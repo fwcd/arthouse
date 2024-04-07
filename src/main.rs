de24b80eb65c10273308c0c41120bc28756bb93c
fwcd <fwcd@fwcd.dev>
2024-04-07 21:58:54 +0200

    Bind a UDP socket on port 6454 and parse Art-Net packets

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
+use artnet_protocol::ArtCommand;
 use clap::Parser;
 use lighthouse_client::{protocol::Authentication, Lighthouse, LIGHTHOUSE_URL};
+use tokio::net::UdpSocket;
+use tracing::{debug, info};
 
 #[derive(Parser)]
 #[command(version)]
@@ -25,5 +28,15 @@ async fn main() -> Result<()> {
     let auth = Authentication::new(&args.username, &args.token);
     let lh = Lighthouse::connect_with_tokio(auth).await?;
 
-    Ok(())
+    let socket = UdpSocket::bind(("0.0.0.0", 6454)).await?;
+
+    info!("Listening for Art-Net packets on {} (UDP)", socket.local_addr()?);
+    loop {
+        // TODO: Handle errors
+        let mut buffer = [0u8; 1024];
+        let (length, addr) = socket.recv_from(&mut buffer).await?;
+        let command = ArtCommand::from_buffer(&buffer[..length])?;
+
+        debug!(%addr, ?command, "Received command");
+    }
 }
